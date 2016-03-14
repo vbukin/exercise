e23d223beae7eab95351cee029be74f1d4be0997
rksdna <rksdna@mail.ru>
2016-03-14 17:15:26 +0400

    student_list

--- app.c
+++ app.c
@@ -1,76 +1,101 @@
-#include "mylib.h"
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 10
-
-char *main_str = 0;
-int size_main_str = 0;
-
-void str_append(char *str2, int count)
+struct student
 {
-    char *new_str = malloc(size_main_str + count);
-    memccpy(new_str, main_str, sizeof(char), size_main_str);
-    memccpy(new_str + size_main_str, str2, sizeof(char), count);
-    if (main_str)
-        free(main_str);
-    main_str = new_str;
-    size_main_str += count;
+    char name[16];
+    int number;
+    int grade;
+};
+
+struct student *all_students = 0;
+int students_count = 0;
+
+void student_append(const struct student *next_student)
+{
+    struct student *temp_students = malloc((students_count + 1) * sizeof(struct student));
+    memcpy(temp_students, all_students, students_count * sizeof(struct student));
+    memcpy(temp_students + students_count, next_student, sizeof(struct student));
+    students_count++;
+    free(all_students);
+    all_students = temp_students;
 }
 
-int find_max(char *str, int size, int position)
+void students_print(FILE *file)
 {
-    int max_index = position;
-    int i;
-    for (i = position; i < size; i++)
+    int i = students_count;
+    struct student *cs = all_students;
+    while (i--)
     {
-        if (str[i] < str[max_index])
-            max_index = i;
+        fprintf(file, "%d %s %d %d\n", i + 1, cs->name, cs->number, cs->grade);
+        cs++;
     }
-    return max_index;
 }
 
-void array_roll(char *str, int e1, int e2)\
-{
-    char temp;
-    temp = str[e1];
-    str[e1] = str[e2];
-    str[e2] = temp;
-}
-
-
-void sort(char *str, int size)
+void insert_grade(int number, int grade)
 {
     int i;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < students_count; i++)
     {
-        array_roll(str,i ,find_max(str, size, i));
+        if (all_students[i].number == number)
+        {
+            all_students[i].grade = grade;
+            return;
+        }
     }
+    fprintf(stderr, "grade for %d not found\n", number);
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc < 0)
+    FILE *file_of_students;
+    FILE *file_of_grade;
+    FILE *file_of_result;
+
+    if (argc != 4)
     {
-        fprintf(stderr, "to many parameters\n");
+        fprintf(stderr, "need only 3 parametrs, you put %d", argc - 1);
+        return 2;
+    }
+
+    if (!(file_of_students = fopen(argv[1], "r")))
+    {
+        fprintf(stderr, "cant open file %s", argv[1]);
         return 1;
     }
 
-    while (!feof(stdin))
-    {
-        const int size = 10;
-        char buffer[10];
-        int count_char;
-        count_char = fread(buffer, sizeof(char), size, stdin);
-        str_append(buffer, count_char);
-    }
-    sort(main_str, size_main_str);
-    fwrite(main_str, sizeof(char), size_main_str, stdout);
-    printf("[%d]", size_main_str);
-    if (main_str)
-        free(main_str);
+    struct student st;
+    st.grade = 0;
+    while (fscanf(file_of_students, "%15s %d\n", st.name, &st.number) == 2)
+        student_append(&st);
 
+    students_print(stdout);
+    fclose(file_of_students);
+
+    if (!(file_of_grade = fopen(argv[2], "r")))
+    {
+        fprintf(stderr, "cant open file %s", argv[2]);
+        return 3;
+    }
+
+    int number;
+    int grade;
+    while (fscanf(file_of_grade, "%d %d\n", &number, &grade) == 2)
+        insert_grade(number, grade);
+
+    students_print(stdout);
+    fclose(file_of_grade);
+
+    if (!(file_of_result = fopen(argv[3], "w")))
+    {
+        fprintf(stderr, "cant write to %s", argv[3]);
+        return 5;
+    }
+
+    students_print(file_of_result);
+    fclose(file_of_result);
+
+    free(all_students);
     return 0;
 }
-
