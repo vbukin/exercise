f3cb70f20682547009ed95a69f6aa800ed099403
rksdna <rksdna@mail.ru>
2016-03-17 16:19:24 +0400

    qx

--- app.c
+++ app.c
@@ -7,79 +7,86 @@ struct st_student
     char name[16];
     double grade_sum;
     int grade_count;
-
 };
 
 int students_count = 0;
+struct st_student *all_students = 0;
 
-int find_position_student(const struct st_student *student, char name)
+int find_position_student(const char *name)
 {
     int i = students_count;
     while (i--)
     {
-        if (strcmp(student[i].name, &name));
-        return i;
+        if (!strcmp(all_students[i].name, name))
+            return i;
     }
     return -1;
 }
 
-void student_append(const struct st_student *next_student, struct st_student **current_students)
+void student_append(const struct st_student *next_student)
 {
-    int i = find_position_student(current_students, next_student->name);
-    printf ("1");
-    if ( i != -1)
+    int i = find_position_student(next_student->name);
+    if (i != -1)
     {
-
-        current_students[i]->grade_sum += next_student->grade_sum;
-        current_students[i]->grade_count++;
+        all_students[i].grade_sum += next_student->grade_sum;
+        all_students[i].grade_count++;
         return;
     }
-    printf ("2");
+
     struct st_student *temp_student = malloc((students_count + 1) * sizeof(struct st_student));
-    memcpy(temp_student, current_students, students_count * sizeof(struct st_student));
-    //    memcpy(temp_student + students_count, next_student, sizeof(struct student));
+    memcpy(temp_student, all_students, students_count * sizeof(struct st_student));
+    memcpy(temp_student + students_count, next_student, sizeof(struct st_student));
     students_count++;
-    printf ("%d", students_count);
-    free(*current_students);
-    *current_students = temp_student;
+    free(all_students);
+    all_students = temp_student;
 }
 
-print_list_of_students(const struct st_student *students)
+void print_list_of_students(FILE *file)
 {
     int i = students_count;
     while (i--)
-        printf ("%s 123", students[i].name);
+        fprintf (file, "%s: %lf: %lf\n", all_students[i].name, all_students[i].grade_sum, all_students[i].grade_sum/all_students[i].grade_count);
 }
 
 int main(int argc, char *argv[])
 {
-    FILE *current_file;
-
-    struct st_student *all_students;
-    struct st_student student;
-
-    /*  if (argc < 3)
+    if (argc < 3)
     {
         fprintf(stderr, "need choise two or more files, you put %d", argc - 1);
+        return 1;
     }
-*/
+
     int i;
-    for (i = 1; i < argc; i++)
+    for (i = 1; i < argc - 1; i++)
     {
+        FILE *current_file;
+        struct st_student student;
+        student.grade_count = 1;
+
         if (!(current_file = fopen(argv[i], "r")))
         {
             fprintf(stderr, "cant open file %s", argv[i]);
-            return 1;
+            return 2;
         }
 
-        while (fscanf(current_file, "%15s %l", student.name, student.grade_sum) == 2)
+        while (fscanf(current_file, "%15s %lf", student.name, &student.grade_sum) == 2)
         {
-            student_append(&student, &all_students);
+            student_append(&student);
         }
 
-
-
+        fclose(current_file);
     }
-    print_list_of_students(all_students);
+
+    FILE *file_out;
+    if ((file_out = fopen(argv[i], "w")))
+    {
+        print_list_of_students(file_out);
+        fclose(file_out);
+    }
+    else
+    {
+        fprintf(stderr, "cant open file %s", argv[i]);
+        return 3;
+    }
     return 0;
 }
